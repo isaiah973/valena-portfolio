f9d707ee8a18e943544d52cc401941cf8d978176
isaiah973 <adeolaisaiah01@gmail.com>
2026-05-04 23:56:18 +0100

    grid menu 2

--- src/Components/MenuSection.jsx
+++ src/Components/MenuSection.jsx
@@ -41,7 +41,7 @@ export default function MenuSection() {
         </motion.div>
 
         {/* GRID */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {menus.map((img, index) => (
             <motion.div
               key={index}
